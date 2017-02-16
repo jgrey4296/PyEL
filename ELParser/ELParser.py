9dd66cb8aa4623334515238479e13b4aec05e969
John Grey <jgrey@ucsc.edu>
2017-02-16 20:29:38 +0000

    cleand up grammar

--- ELParser/ELParser.py
+++ ELParser/ELParser.py
@@ -28,10 +28,10 @@ import IPython
 s = pp.Suppress
 
 #Allows management of Components in the parse, but remember to wrap in str()
-#Not intended to be human usable
-PARSENAMES = Enum('PARSENAMES','BASEFACT ARRAY FACT')
+#Not intended to be human usable, or anywhere other than the parser.
+PARSENAMES = Enum('PARSENAMES','BASEFACT ARRAY FACT TERMINAL ROOT')
 
-#Human usable names:
+#Human usable names of the parser:
 FACTNAME = "Fact"
 
 
@@ -39,14 +39,16 @@ FACTNAME = "Fact"
 def debugPA(toks):
     IPython.embed(simple_prompt=True)
 
+#ELBD construction functions, only intended to be used in the parser 
 def construct_el_fact(toks):
     if str(PARSENAMES.BASEFACT) not in toks:
         raise Exception('No BaseFact provided',toks)
-    if str(PARSENAMES.ARRAY) in toks:
-        a = toks[str(PARSENAMES.ARRAY)][0]
-    else:
-        a = None
-    return t_ELFACT(toks[str(PARSENAMES.BASEFACT)][:],a)
+    root = [toks[0]]
+    base = toks[str(PARSENAMES.BASEFACT)][:]
+    term = [toks[-1]]
+    return ELBD.ELFACT(root + base + term)
+
+
 
 def construct_num(toks):
     underscore_removed = toks.replace('_','')
@@ -81,20 +83,27 @@ ARRAY = s(pp.Literal('[')) + pp.Optional(s(pp.Optional(pp.LineEnd()))
                                                            + pp.Optional(pp.LineEnd()))
                                                          + ELEMENT)) \
                     + pp.Suppress(pp.Literal(']'))
+ARRAY.setParseAction(lambda toks: [toks[:]])
 #The final part of a fact can be an array:
-EL_ARRAY = (DOT | EX) + ARRAY
-EL_ARRAY.setParseAction(lambda tok: t_ELARRAY(tok[0],tok[1:]))
-EL_PAIR = (DOT | EX) + ELEMENT
-EL_PAIR.setParseAction(lambda tok: t_ELPAIR(tok[0],tok[1]))
+EL_ARRAY = ARRAY
+#Core part of a fact:
+EL_PAIR = ELEMENT + pp.NotAny(pp.LineEnd()) + (DOT | EX)
+EL_PAIR.setParseAction(lambda tok: ELBD.ELPAIR(tok[0],tok[1]))
+#Root part of a fact:
+EL_FACT_ROOT = pp.Group(DOT)
+EL_FACT_ROOT.setParseAction(lambda tok: ELBD.ELROOT(ELBD.EL.DOT))
+EL_FACT_TERMINAL = pp.Group(ELEMENT | EL_ARRAY)
+#tok[0][0] for the group wrapping then element wrapping
+EL_FACT_TERMINAL.setParseAction(lambda tok: ELBD.ELTERM(tok[0][0]))
 
 #An Entire sequence, note the stopOn to not continue over lines
-FACT = pp.Group(pp.OneOrMore(EL_PAIR,stopOn=pp.LineEnd())).setResultsName(str(PARSENAMES.BASEFACT)) + pp.Optional(EL_ARRAY).setResultsName(str(PARSENAMES.ARRAY))
+FACT = EL_FACT_ROOT + pp.Group(pp.ZeroOrMore(EL_PAIR)).setResultsName(str(PARSENAMES.BASEFACT)) + EL_FACT_TERMINAL
 FACT.setParseAction(lambda toks: construct_el_fact(toks))
 FACT.setResultsName(FACTNAME)
 
 
 #The entire grammar:
-ROOT = pp.OneOrMore(FACT + s(pp.Optional(pp.LineEnd() | pp.StringEnd()))).ignore(COMMENTS)
+ROOT = pp.OneOrMore(FACT + s(pp.LineEnd() | pp.StringEnd())).ignore(COMMENTS)
 
 
 #TODO: SET FAIL ACTIONS: .setFailAction(lambda s,loc,expr,err: 0)
@@ -102,5 +111,12 @@ ROOT = pp.OneOrMore(FACT + s(pp.Optional(pp.LineEnd() | pp.StringEnd()))).ignore
 
 ########################################
 if __name__ == "__main__":
+    LOGLEVEL = root_logger.DEBUG
+    LOG_FILE_NAME = "genTest.log"
+    root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
+    console = root_logger.StreamHandler()
+    console.setLevel(root_logger.INFO)
+    root_logger.getLogger('').addHandler(console)
+    logging = root_logger.getLogger(__name__)
     logging.info("Starting ")
     
