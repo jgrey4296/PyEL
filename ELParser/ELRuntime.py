7b686d577d2b9d1154bbc29e593ec90696d71050
John Grey <jgrey@ucsc.edu>
2017-02-16 20:28:14 +0000

    initial commit of runtime file

---
+++ ELParser/ELRuntime.py
@@ -0,0 +1,17 @@
+"""
+	A Trie based runtime for EL
+"""
+import logging as root_logger
+logging = root_logger.getLogger(__name__)
+
+
+from enum import Enum
+from collections import namedtuple
+from fractions import Fraction
+import IPython
+import ELParser
+import ELTrie
+
+
+
+
