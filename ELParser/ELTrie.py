03a37eb31a8e353527a55e48042dce27b299e4b7
John Grey <jgrey@ucsc.edu>
2017-02-17 18:29:27 +0000

    added some logging statements, and moved ELTrieNode out of the file

--- ELParser/ELTrie.py
+++ ELParser/ELTrie.py
@@ -3,60 +3,9 @@ A Simple Trie for EL
 """
 import logging as root_logger
 from ELParser import ELBaseData as ELBD
+from ELParser.ELBaseData import ELTrieNode
 logging = root_logger.getLogger(__name__)
 
-class ELTrieNode:
-    """ The internal node used for the Trie.
-    Nominally an EL Operator (DOT or EX), and a value, usually a dict 
-    """
-    
-    def __init__(self,val):
-        self.elop = ELBD.EL.DOT
-        self.value = None
-        self.children = {}
-        if isinstance(val,ELBD.ELPAIR):
-            self.elop = val.elop
-            self.value = val.value
-        elif isinstance(val,ELBD.ELTERM):
-            self.value = val.value
-        else:
-            self.value = val
-
-    def __repr__(self):
-        return "EL_Trie_Node({},{})".format(str(self.value),str(self.elop))
-            
-    def is_empty(self):
-        return len(self.children) == 0
-        
-    def elops_match(self,el_op):
-        return False
-
-    def __getitem__(self,key):
-        if isinstance(key,ELBD.ELPAIR):
-            return self.children[key.value]
-        elif isinstance(key,ELBD.ELTERM):
-            return self.children[key.value]
-        else:
-            return self.children[key]
-
-    def __setitem__(self,key,value):
-        if isinstance(key,ELBD.ELPAIR):
-            self.children[key.value] = value
-        elif isinstance(key,ELBD.ELTERM):
-            self.children[key.value] = value
-        else:
-            self.children[key] = value
-            
-    def __contains__(self,key):
-        if isinstance(key,ELBD.ELPAIR):
-            return key.value in self.children
-        elif isinstance(key,ELBD.ELTERM):
-            return key.value in self.children
-        else:
-            return key in self.children
-        
-    def keys(self):
-        return self.children.keys()
 
     
 class ELTrie:
@@ -73,6 +22,7 @@ class ELTrie:
         """ Take an ELFact of [ROOT, [PAIRS], TERM],
         and attempt to add to the trie
         """
+        logging.debug("Starting to push: {}".format(str(el_string)))
         assert isinstance(el_string,ELBD.ELFACT)
         assert isinstance(el_string.data[0],ELBD.ELROOT)
         if len(el_string.data) > 1:
@@ -83,12 +33,16 @@ class ELTrie:
         current = None
         for statement in el_string:
             if isinstance(statement,ELBD.ELROOT):
+                logging.debug("Hit Root")
                 current = self.root
                 continue
             elif isinstance(statement,ELBD.ELTERM) and statement not in current:
+                logging.debug("Missing TERM: {}".format(str(statement)))
                 current[statement] = ELTrieNode(statement)
             elif isinstance(statement,ELBD.ELPAIR) and statement not in current:
+                logging.debug("Missing PAIR: {}".format(str(statement)))
                 current[statement] = ELTrieNode(statement)
+            logging.debug("-> {}".format(str(statement)))
             current = current[statement]
             
         return ELBD.ELSuccess()
