f7d34700e92456995ab1fb82f63ae75a50814654
John Grey <jgrey@ucsc.edu>
2017-02-19 11:44:53 +0000

    improved logging for parser tests

--- tests/ELParser_tests.py
+++ tests/ELParser_tests.py
@@ -291,5 +291,14 @@ class ELParser_Tests(unittest.TestCase):
     
         
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    LOGLEVEL = root_logger.DEBUG
+    LOG_FILE_NAME = "ELParser_tests.log"
+    root_logger.basicConfig(filename=LOG_FILE_NAME, level=LOGLEVEL, filemode='w')
+    console = root_logger.StreamHandler()
+    console.setLevel(root_logger.INFO)
+    root_logger.getLogger('').addHandler(console)
+    logging = root_logger.getLogger(__name__)
+    root_logger.disable(root_logger.CRITICAL)
+    ##############################
+
     unittest.main()
