29f1f80800783e02ae433f3038f71cc39ac1d9e0
John Grey <jgrey@ucsc.edu>
2017-02-24 00:01:16 +0000

    changed from generic parseString to custom utility parse function

--- tests/ELParser_tests.py
+++ tests/ELParser_tests.py
@@ -18,13 +18,13 @@ gen_n = lambda: 1 + int(random()*20)
 class ELParser_Tests(unittest.TestCase):
 
     def setUp(self):
-        self.parser = ELParser.ROOT
+        self.parser = ELParser.ELPARSE
     def tearDown(self):
         self.parser = None
  
     def test_simple(self):
         """ Check the parser works in a minimal case """
-        result = self.parser.parseString('.this.is.a.test')[0]
+        result = self.parser('.this.is.a.test')[0]
         self.assertIsInstance(result,ELBD.ELFACT)
 
     def test_n_facts(self):
@@ -32,13 +32,13 @@ class ELParser_Tests(unittest.TestCase):
         n_facts = gen_n()
         fact_string = ".this.is.a.test"
         all_facts = "\n".join([fact_string for x in range(n_facts)])
-        results = self.parser.parseString(all_facts)
+        results = self.parser(all_facts)
         self.assertEqual(len(results),n_facts)
 
     def test_results_are_ELFACTS(self):
         """ check that the returned type is a ELBD.ELFACT """
         fact_string = ".this.is.a.test\n.this.is.another.test"
-        results = self.parser.parseString(fact_string)
+        results = self.parser(fact_string)
         self.assertIsInstance(results[0],ELBD.ELFACT)
         self.assertIsInstance(results[1],ELBD.ELFACT)
 
@@ -46,14 +46,14 @@ class ELParser_Tests(unittest.TestCase):
         """ check that a parsed result contains the correct amount of data in its data field """
         fact_length = gen_n()
         test_fact = ".test".join(["" for x in range(fact_length)])
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         #-1 because you're comparing to the *connections* not the actual elements in the empty array
         self.assertEqual(len(results[0].data),fact_length)
 
     def test_results_contain_array(self):
         """ check that the parsed result correctly gets an array in a fact """
         test_fact = ".this.is.an.array.[1,2,3,4,5]"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].data),6)
         self.assertIsInstance(results[0].data[-1],ELBD.ELTERM)
         self.assertEqual(results[0].data[-1].value,[1,2,3,4,5])
@@ -61,7 +61,7 @@ class ELParser_Tests(unittest.TestCase):
     def test_empty_array(self):
         """ check that an array can be empty """
         test_fact = ".this.is.an.empty.array.[]"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         #length is 7, num of '.' + terminal
         self.assertEqual(len(results[0].data),7)
         self.assertIsInstance(results[0].data[-1],ELBD.ELTERM)
@@ -74,7 +74,7 @@ class ELParser_Tests(unittest.TestCase):
         2,
         3
         ]"""
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].data),7)
         self.assertIsInstance(results[0].data[-1],ELBD.ELTERM)
         #Checking LENGTH:
@@ -85,82 +85,82 @@ class ELParser_Tests(unittest.TestCase):
     def test_fact_with_string_inside(self):
         """ check facts can have strings inside them """
         test_fact = '.this.is.a."string fact"'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,'"string fact"')
 
     def test_fact_with_string_sub_values(self):
         """ check that a string fact can continue having sub values """
         test_fact = '.this.is."a test".with.subvalues'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].data),6)
 
     def test_fact_with_exclusion_operator(self):
         """ Check the ! exclusion operator works in facts """
         test_fact = ".this.is.an!exclusive.fact"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].data),6)
         self.assertEqual(results[0].data[3].elop,ELBD.EL.EX)
 
     def test_fact_with_string_including_ex_op(self):
         """ check that an ! in a string doesn't interfere """
         test_fact = '.this.is."a !test"'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].data),4)
         self.assertEqual(results[0].data[-1].value,'"a !test"')
 
     def test_fact_with_number(self):
         """ check that facts can include numbers """
         test_fact = '.a.b.5'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,5)
 
     def test_fact_with_negative_number(self):
         """ check that numbers in facts can be negative """
         test_fact = '.a.b.-5'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,-5)
 
     def test_fact_with_underscore_number(self):
         """ check that numbers can be formatted to be read """
         test_fact = '.a.b.5_000_000'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,5000000)
 
     def test_fact_with_number_array(self):
         """ check that numbers can be in arrays """
         test_fact ='.a.b.[1,2,3]'
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,[1,2,3])
 
     def test_fact_with_underscore_negative_number(self):
         """ check that formatted numbers can be negative """
         test_fact = ".a.b.-12_000_000"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,-12000000)
 
     def test_facts_can_be_fractions(self):
         """ check that numbers in facts can be fractions """
         test_fact = ".a.b.1/5"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertIsInstance(results[0].data[-1].value,Fraction)
 
     def test_facts_can_be_decimals(self):
         """ check that numbers can be decimals """
         test_fact = ".a.b.1d5"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(results[0].data[-1].value,1.5)
 
     def test_numbers_can_have_sub_facts(self):
         """ make sure numbers dont have to be leaves """
         test_fact = ".a.b.5.c"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].data),5)
         self.assertEqual(results[0].data[-1].value,'c')
 
     def test_comments_are_ignored(self):
         """ make sure comments are ignored """
         test_fact = ".a.b.c\n#blahhhhh\n.c.d.e"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results),2)
 
     ####################
@@ -168,7 +168,7 @@ class ELParser_Tests(unittest.TestCase):
     def test_empty_rule(self):
         """ test .this.is.a.rule.{[] -> []} """
         test_fact = ".this.is.a.rule.{[]->[]} """
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results),1)
         self.assertIsInstance(results[0],ELBD.ELFACT)
         self.assertEqual(len(results[0]),5)
@@ -202,7 +202,7 @@ class ELParser_Tests(unittest.TestCase):
         [.d,.e,.f]
         }
         """
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertIsInstance(results[0][-1].value,ELBD.ELRULE)
         for x in results[0][-1].value.conditions:
             self.assertIsInstance(x,ELBD.ELFACT)
@@ -213,7 +213,7 @@ class ELParser_Tests(unittest.TestCase):
     def test_rule_multiconditions(self):
         """ Test .this.is.a.rule{[.blah.bloo,.blee.blah] -> [.wee.bloo, .wee.blah]} """
         test_fact = ".this.is.a.rule.{[.blah.bloo,.blee.blah] -> [.wee.bloo, .wee.blah]}"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertIsInstance(results[0][-1].value,ELBD.ELRULE)
         for x in results[0][-1].value.conditions:
             self.assertIsInstance(x,ELBD.ELFACT)
@@ -227,7 +227,7 @@ class ELParser_Tests(unittest.TestCase):
     		[.blah.bloo,.blee.blah] 
         	-> 
 		[.wee.bloo, .wee.blah]}"""
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertIsInstance(results[0][-1].value,ELBD.ELRULE)
         for x in results[0][-1].value.conditions:
             self.assertIsInstance(x,ELBD.ELFACT)
@@ -238,21 +238,21 @@ class ELParser_Tests(unittest.TestCase):
     def test_rule_bindings(self):
         """ Test .this.is.a.rule.{[.blah.bloo.$1] -> [.bloo.blee.$1]} """
         test_fact = ".this.is.a.rule.{[.blah.bloo.$1] -> [.bloo.blee.$1]}"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertIsInstance(results[0][-1].value.conditions[0][-1].value, ELBD.ELVAR)
         self.assertIsInstance(results[0][-1].value.actions[0][-1].value,ELBD.ELVAR)
 
     def test_bindings_are_registered(self):
         """ bindings need to be registered with their parent fact, and that facts parent rule """
         test_fact = ".this.is.a.$binding"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].bindings),1)
         self.assertEqual(results[0].bindings[0].value,"binding")
 
     def test_bindings_are_registered_when_non_terminal(self):
         """ bindings should be registered even when they aren't at the end of a fact """
         test_fact = ".this.is.a.$binding.blah"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0].bindings),1)
         self.assertEqual(results[0].bindings[0].value,"binding")
 
@@ -260,12 +260,12 @@ class ELParser_Tests(unittest.TestCase):
         """ undefined bindings in actions should complain """
         test_fact = ".this.is.a.rule.{[.blah.bloo] -> [.blah.bloo.$blee]}"
         with self.assertRaises(ELE.ELConsistencyException):
-            self.parser.parseString(test_fact)
+            self.parser(test_fact)
 
     def test_fact_negation(self):
         """ ~.a.fact.should.be.negatable """
         test_fact = "~.a.fact.should.be.negatable """
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertIsInstance(results[0][-1],ELBD.ELTERM)
         self.assertEqual(results[0][-1].value,"negatable")
         self.assertTrue(results[0].negated)
@@ -273,26 +273,26 @@ class ELParser_Tests(unittest.TestCase):
     def test_fact_not_negated(self):
         """ .a.normal.fact.shouldnt.be.negated """
         test_fact = ".a.normal.fact.shouldnt.be.negated """
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertFalse(results[0].negated)
             
     def test_rule_negation_testing(self):
         """ Test .this.is.a.rule.{[~.blah.bloo.blee] -> [.blah.bloo.blee]} """
         test_fact = ".this.is.a.rule.{[~.blah.bloo.blee] -> [.blah.bloo.blee]}"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertTrue(results[0][-1].value.conditions[0].negated)
         self.assertFalse(results[0][-1].value.actions[0].negated)
 
     def test_rule_action_negation(self):
         """ test .this.is.a.rule.{[] -> [~.blah.bloo.blee]} """
         test_fact = ".this.is.a.rule.{[] -> [~.blah.bloo.blee]}"
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertTrue(results[0][-1].value.actions[0].negated)
 
     def test_rule_condition_comparison(self):
         """ .this.is.a.rule.{[.a.b.$c, .a.b.$d] | [$c > $d ] -> [] } """
         test_fact = ".this.is.a.rule.{[.a.b.$c, .a.b.$d] | [$c > $d] -> [] } "
-        results = self.parser.parseString(test_fact)
+        results = self.parser(test_fact)
         self.assertEqual(len(results[0][-1].value.binding_comparisons),1)
         self.assertEqual(results[0][-1].value.binding_comparisons[0].b1.value,'c')
         self.assertEqual(results[0][-1].value.binding_comparisons[0].b2.value,'d')
