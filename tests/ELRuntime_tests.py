e53f85ecd687245dd2ca2da2dbb463e48286e002
John Grey <jgrey@ucsc.edu>
2017-02-25 20:35:08 +0000

    added runtime tests

--- tests/ELRuntime_tests.py
+++ tests/ELRuntime_tests.py
@@ -9,22 +9,143 @@ from test_context import ELParser
 from ELParser import ELParser
 from ELParser import ELBaseData as ELBD
 from ELParser import ELExceptions as ELE
+from ELParser import ELRuntime as ELR
 from fractions import Fraction
 
 #Parser returns a ParseResult, which is an array of actual parse data structures
 
 gen_n = lambda: 1 + int(random()*20)
 
-class ELParser_Tests(unittest.TestCase):
+class ELRuntime_Tests(unittest.TestCase):
 
     def setUp(self):
-        self.runtime = None
+        self.runtime = ELR.ELRuntime()
     def tearDown(self):
         self.runtime = None
  
     def test_simple(self):
         """ Check the runtime works in a minimal case """
+        self.assertIsNotNone(self.runtime)
+        self.assertIsInstance(self.runtime,ELR.ELRuntime)
+
+    def test_simple_fact_assertion(self):
+        test_fact = ".this.is.a.test"
+        self.runtime(test_fact)
+        check_fact = ".this.is.a"
+        parsed = self.runtime.parser(check_fact)[0]
+        retrieved = self.runtime.trie.get(parsed)
+        self.assertTrue(retrieved)
+        self.assertEqual(retrieved,'a')
+        self.assertIn('test',retrieved)
+
+
+    def test_simple_fact_query(self):
+        test_fact = ".this.is.a.test"
+        self.runtime(test_fact)
+        self.assertTrue(self.runtime.query(test_fact + "?"))
+
+    def test_simple_fact_query_fail(self):
+        test_fact = ".this.hasnt.been.added"
+        self.assertFalse(self.runtime.query(test_fact + "?"))
+        
+    def test_multi_fact_assertion(self):
+        facts = [".this.is.a.test", ".this.is.another.test", ".and.a.third"]
+        self.runtime("\n".join(facts))
+        for fact in facts:
+            self.assertTrue(self.runtime.query(fact + "?"))
+
+    def test_multi_query(self):
+        facts = [".this.is.a.test", ".this.is.another.test", ".and.a.third"]
+        self.runtime("\n".join(facts))
+        as_queries = [x + "?" for x in facts]
+        self.assertTrue(self.runtime.query("\n".join(as_queries)))
+
+            
+    def test_metrics_max_depth(self):
         None
+
+    def test_metrics_num_leaves(self):
+        None
+
+    def test_metrics_num_rules(self):
+        None
+
+    def test_metrics_num_assertions(self):
+        None
+
+    def test_metrics_num_retractions(self):
+        None
+        
+        
+    def test_simple_fact_retraction(self):
+        None
+        
+    def test_exclusion_semantics(self):
+        None
+
+    def test_rule_definition(self):
+        None
+
+    def test_rule_firing(self):
+        None
+
+    def test_rule_binding(self):
+        None
+
+    def test_rule_arith_action(self):
+        None
+
+    def test_fact_arrays(self):
+        None
+
+    def test_string_interpolation(self):
+        None
+
+    def test_fact_arithmetic(self):
+        None
+
+    def test_comments_are_ignored(self):
+        None
+
+    def test_rule_binding_balances(self):
+        None
+
+    def test_rule_comparisons(self):
+        None
+
+    def test_rule_binding_arith(self):
+        None
+
+    def test_rule_implicit_binding_comparison(self):
+        None
+
+    def test_rule_binding_non_equality(self):
+        None
+
+    def test_rule_near_operator(self):
+        None
+
+    def test_rule_near_operator_with_binding(self):
+        None
+
+    def test_global_binding(self):
+        None
+
+    def test_global_rebinding(self):
+        None
+
+    def test_subtree_testing(self):
+        None
+
+    def test_subtree_application(self):
+        None
+
+    def test_subtree_exclusive_application(self):
+        None
+
+    def test_subtree_variable_application(self):
+        None
+        
         
 if __name__ == "__main__":
     LOGLEVEL = root_logger.DEBUG
